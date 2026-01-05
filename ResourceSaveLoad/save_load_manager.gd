529b03b8bbd9f41069b810164145df478776c562
Luyuan Zhu <122760148+AbsoluteZer0527@users.noreply.github.com>
2026-01-04 17:57:53 -0800

    Saving boxes in the scene into the save system

--- ResourceSaveLoad/save_load_manager.gd
+++ ResourceSaveLoad/save_load_manager.gd
@@ -7,6 +7,12 @@ func _save() -> void:
 	data.player_position = player.global_position
 	data.is_facing_left = player.get_child(0).flip_h;
 	
+	var boxes = get_tree().get_nodes_in_group("Box")
+	for box in boxes:
+		var box_scene = PackedScene.new()
+		box_scene.pack(box)
+		data.box_array.append(box_scene)
+		
 	ResourceSaver.save(data, "user://scene_data.tres")
 	print("saved!")
 
@@ -15,5 +21,6 @@ func _load() -> void:
 	var data = ResourceLoader.load("user://scene_data.tres") as SceneData
 	player.global_position = data.player_position
 	player.get_child(0).flip_h = data.is_facing_left
+	
 	
 	print("loaded!")
--- ResourceSaveLoad/scene_data.gd
+++ ResourceSaveLoad/scene_data.gd
@@ -3,3 +3,5 @@ class_name SceneData
 
 @export var player_position : Vector2
 @export var is_facing_left : bool
+
+@export var box_array : Array[PackedScene]
