77366c41c117e4bf13beb0a0d92a610148ed8773
Luyuan Zhu <122760148+AbsoluteZer0527@users.noreply.github.com>
2026-01-04 18:01:21 -0800

    code for reload the boxes onto the game.

--- ResourceSaveLoad/save_load_manager.gd
+++ ResourceSaveLoad/save_load_manager.gd
@@ -3,10 +3,12 @@ extends CanvasLayer
 @export var player : Node2D
 
 func _save() -> void:
+	#save player
 	var data = SceneData.new()
 	data.player_position = player.global_position
 	data.is_facing_left = player.get_child(0).flip_h;
 	
+	#save boxes 
 	var boxes = get_tree().get_nodes_in_group("Box")
 	for box in boxes:
 		var box_scene = PackedScene.new()
@@ -18,9 +20,17 @@ func _save() -> void:
 
 
 func _load() -> void:
+	#load player
 	var data = ResourceLoader.load("user://scene_data.tres") as SceneData
 	player.global_position = data.player_position
 	player.get_child(0).flip_h = data.is_facing_left
 	
+	#remove current unsaved boxes
+	for box in get_tree().get_nodes_in_group("Box"):
+		box.queue_free()
 	
+	for box in data.box_array:
+		var box_node = box.instantiate()
+		get_tree().current_scene.add_child(box_node)
+		
 	print("loaded!")
